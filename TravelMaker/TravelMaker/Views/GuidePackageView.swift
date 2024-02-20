d79efaa4fb098dd3c928131ecfaa199d84a308a8
UDI94 <145957641+UDI94@users.noreply.github.com>
2024-02-21 08:59:39 +0900

    카테고리및 필터 추가

--- TravelMaker/TravelMaker/Views/GuidePackageView.swift
+++ TravelMaker/TravelMaker/Views/GuidePackageView.swift
@@ -8,60 +8,86 @@
 import SwiftUI
 
 struct GuidePackageView: View {
-//    var travelModel: TravelModel
+    
+    var tagIndex : Int
     let userStore = UserModelStore().userStore
-    let travel = TravelModelStore().travelStore
+    var travels: [TravelModel] = TravelModelStore().travelStore
     
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("게스트 선호 가이드 특별추천")
-                .font(.system(size: 25))
-                .fontWeight(.heavy)
+        ScrollView{
             
-            Image(travel[0].imageName[0]) // 여행지 사진
-                .resizable()
-                .aspectRatio(contentMode: .fill)
-                .frame(height: 200)
-                .cornerRadius(10)
-            
-            HStack {
-                Image(travel[0].guideProfileImage) // 가이드 프로필 이미지
-                    .resizable()
-                    .frame(width: 50, height: 50)
-                    .clipShape(Circle())
-                    .overlay {
-                        Circle().stroke(.gray, lineWidth: 0.2)
+                VStack(alignment: .leading) {
+                    ForEach(filteredData(index: tagIndex)) {travel in
+
+                            VStack (alignment: .leading){
+                                Image(travel.imageName) // 여행지 사진
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fill)
+                                    .frame(height: 200)
+                                    .cornerRadius(10)
+                                    .padding(.vertical, 10.0)
+                                
+                                Section{
+                                    HStack {
+                                        Image(travel.guideProfileImage) // 가이드 프로필 이미지
+                                            .resizable()
+                                            .frame(width: 50, height: 50)
+                                            .clipShape(Circle())
+                                            .overlay {
+                                                Circle().stroke(.gray, lineWidth: 0.2)
+                                            }
+                                            .shadow(radius: 5)
+                                            .padding(.bottom, 10.0)
+
+                                        VStack (alignment: .leading) {
+                                            Text("\(travel.author.name) 가이드님의 아름다운 휴양지") // 가이드 이름
+                                                .font(.headline)
+                                                .padding(.leading, 10.0)
+                                            Text("￦ \(travel.price ?? 0)") // 가격
+                                                .fontWeight(.bold)
+                                        }
+                                        Spacer()
+                                    }
+                                }
+                                
+                                Section{
+                                    Text(travel.title) // 피드 제목
+                                        .font(.title2)
+                                        .fontWeight(.bold)
+                                    
+                                    VStack(alignment: .leading, spacing: 25) {
+                                        Text("위치: \(travel.location)") // 위치
+                                            .font(.subheadline)
+                                        
+                                        Text(travel.description) // 설명
+                                            .font(.body)
+                                            .lineLimit(2)
+                                    }
+                                    
+                                }
+                            }
+                            Divider()
+                            
+                        }
                     }
-                    .shadow(radius: 5)
-                
-                Text("\(travel[0].author.name) 가이드님의 아름다운 휴양지") // 가이드 이름
-                    .font(.headline)
-                Spacer()
-                Text("￦ \(travel[0].price ?? 0)") // 가격
-                    .fontWeight(.bold)
+                }
+                .padding()
+                .background(Color.white)
+                .cornerRadius(10)
+                .shadow(radius: 2)
+                .foregroundColor(Color.tmBlack)
             }
-            
-            Text(travel[0].title) // 피드 제목
-                .font(.title2)
-                .fontWeight(.bold)
-            
-            VStack(alignment: .leading, spacing: 25) {
-                Text("위치: \(travel[0].location)") // 위치
-                    .font(.subheadline)
-                
-                Text(travel.description) // 설명
-                    .font(.body)
+        
+    
+    func filteredData(index: Int) -> [TravelModel] {
+            if index > 0 {
+                return travels.filter { $0.tag == Tag.allCases[index] }
+            } else {
+                return travels
             }
         }
-        .padding()
-        .background(Color.white)
-        .cornerRadius(10)
-        .shadow(radius: 2)
-        .foregroundColor(.black)
-        
-    }
 }
 
 #Preview {
-    GuidePackageView()
+    GuidePackageView(tagIndex: 1)
 }
--- TravelMaker/TravelMaker/Views/HomeView.swift
+++ TravelMaker/TravelMaker/Views/HomeView.swift
@@ -8,11 +8,27 @@
 import SwiftUI
 
 struct HomeView: View {
+    let travelStore: [TravelModel] = TravelModelStore().travelStore
     var body: some View {
-        VStack {
-//            SearchBar() // 검색
-//            Spacer()
-            CategoryView()
+        VStack{
+            VStack {
+                Text("Travel Maker")
+                    .font(.largeTitle)
+                    .foregroundStyle(Color.tmBlue2)
+                SearchBar() // 검색
+                    .padding(.bottom, 20.0)
+            }
+            .padding(.horizontal)
+            Divider()
+            VStack(alignment: .leading){
+                Text("게스트들이 선호하는 가이드 상품")
+                    .font(.title2)
+                    .fontWeight(.heavy)
+                    .padding(.leading , 20.0)
+                    .padding(.top, 10.0)
+                CategoryView()
+            }
+            
             
         }
     }
