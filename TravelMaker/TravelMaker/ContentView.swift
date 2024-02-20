6133a746bd0fe1a51f06a75937f4a8dcc8e9dd9f
Wooseok Lee <wl00ie19@gmail.com>
2024-02-20 09:09:56 +0900

    빈 프로젝트파일 생성

---
+++ TravelMaker/TravelMaker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TravelMaker
+//
+//  Created by 이우석 on 2/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TravelMaker/TravelMaker/TravelMakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelMakerApp.swift
+//  TravelMaker
+//
+//  Created by 이우석 on 2/20/24.
+//
+
+import SwiftUI
+
+@main
+struct TravelMakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
