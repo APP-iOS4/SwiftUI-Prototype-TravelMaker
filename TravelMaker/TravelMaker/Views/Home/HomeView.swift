5193e4f847d282f336fe1a5fb31de739e432331d
rlvy0513 <rlvy0513@naver.com>
2024-02-22 15:38:25 +0900

    HomeView 위치 수정

--- TravelMaker/TravelMaker/Views/Home/HomeView.swift
+++ TravelMaker/TravelMaker/Views/Home/HomeView.swift
@@ -6,15 +6,38 @@
 //
 
 import SwiftUI
+import CoreLocation
+
+class LocationManagerDelegate: NSObject, CLLocationManagerDelegate, ObservableObject {
+    @Published var userLocationName: String?
+    private let geocoder = CLGeocoder()
+    
+//    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+//        guard let location = locations.first else { return }
+//        geocoder.reverseGeocodeLocation(location) { placemarks, error in
+//            guard let placemark = placemarks?.first, error == nil else { return }
+//            self.userLocationName = placemark.locality ?? "값 없음"
+//        }
+//    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        // 위치를 가져올 수 없는 경우 기본 위치 설정
+        self.userLocationName = "서울"
+    }
+}
 
 struct HomeView: View {
+    
+    @ObservedObject var locationManagerDelegate = LocationManagerDelegate()
+    @State private var locationManager = CLLocationManager()
+    
     @State var isShowing: Bool = false
     
     @State private var isTagSelected: [Bool] = Array(repeating: false, count: 7)
     private let tagImages: [String] = ["map","fork.knife.circle", "building.columns", "figure.pool.swim", "photo.artframe", "binoculars", "tree"]
     private let tagNames: [String] = ["전체","맛집", "예술", "레저", "포토", "문화", "자연"]
     @State var tagIndex: Int = 0
-   // let travel = TravelModelStore().travelStore
+    // let travel = TravelModelStore().travelStore
     
     let userStore = UserModelStore().userStore
     var travels: [TravelModel] = TravelModelStore().travelStore
@@ -25,9 +48,29 @@ struct HomeView: View {
         NavigationStack {
             VStack{
                 VStack(spacing: 5) {
-                    Text("Travel Maker")
-                        .font(.largeTitle)
-                        .foregroundStyle(Color.tmBlue2)
+                    VStack {
+                        if let userLocationName = locationManagerDelegate.userLocationName {
+                            HStack {
+                                Image(systemName: "mappin.and.ellipse")
+                                
+                                Text("내 위치: \(userLocationName)")
+                            }
+                        }
+                        else {
+                            Text("현재 위치를 불러올 수 없습니다.")
+                        }
+                    }
+                    .onAppear {
+                        self.locationManager.delegate = locationManagerDelegate
+                        self.locationManager.requestWhenInUseAuthorization()
+                        self.locationManager.startUpdatingLocation()
+                    }
+                    .font(.title2)
+                    .fontWeight(.semibold)
+                    .padding(5)
+                    .foregroundStyle(Color.tmBlue2)
+                    
+                    
                     SearchBar() // 검색
                         .padding(.bottom, 3)
                 }
@@ -68,7 +111,15 @@ struct HomeView: View {
                         }
                         //GuidePackageView(tagIndex: tagIndex)
                         VStack {
-                            //Text("제주도에서 많이 찾는 여행 상품")
+                            VStack {
+                                if let userLocationName = locationManagerDelegate.userLocationName {
+                                    Text("\(userLocationName)에서 많이 찾는 여행 상품")
+                                        .font(.title)
+                                        .fontWeight(.semibold)
+                                        .padding(.vertical, 3)
+                                }
+                            }
+                            
                             //MARK: - 여행 항목
                             ForEach(filteredData(index: tagIndex)) {travel in
                                 GuidePackageView(travel: travel, isShowing: isShowing)
@@ -82,7 +133,7 @@ struct HomeView: View {
             .onTapGesture {
                 UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
             }
-        } 
+        }
     }
     
     func filteredData(index: Int) -> [TravelModel] {
