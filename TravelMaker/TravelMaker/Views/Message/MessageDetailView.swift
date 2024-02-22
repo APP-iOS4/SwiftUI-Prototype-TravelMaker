a25470e876baba1cd9d2cb9339308a03a55baeb2
Wooseok Lee <wl00ie19@gmail.com>
2024-02-22 13:10:10 +0900

    fix: 메세지 경고문 추가 및 구조 수정

--- TravelMaker/TravelMaker/Views/Message/MessageDetailView.swift
+++ TravelMaker/TravelMaker/Views/Message/MessageDetailView.swift
@@ -11,61 +11,71 @@ struct MessageDetailView: View {
     var messageModel: MessageModel
     @State var write: String = ""
     @State var sentList: [SingleMessageModel] = []
-    // @State var sentDate: [String] = []
     
     var body: some View {
         VStack(spacing: 0){
             ScrollView{
-                LazyVStack(alignment: .leading){
-                    ForEach(messageModel.messages) { message in
-                        VStack(alignment: .leading){
-                            ZStack(alignment: .leading){
-                                RoundedRectangle(cornerSize: CGSize(width: 10, height: 10))
-                                    .foregroundStyle(.tmBlue1)
-                                Text("\(message.message)")
-                                    .foregroundStyle(.tmBlack)
-                                    .padding(10)
+                
+                LazyVStack{
+                    LazyVStack(alignment: .leading){
+                        ForEach(messageModel.messages) { message in
+                            VStack(alignment: .leading){
+                                ZStack(alignment: .leading){
+                                    RoundedRectangle(cornerSize: CGSize(width: 10, height: 10))
+                                        .foregroundStyle(.tmBlue1)
+                                    Text("\(message.message)")
+                                        .foregroundStyle(.tmBlack)
+                                        .padding(10)
+                                }
+                                .frame(width: 250)
+                                HStack{
+                                    NavigationLink(destination: ProfileView(), label: {
+                                        Image(messageModel.user.profileImage)
+                                            .resizable()
+                                            .scaledToFit()
+                                            .clipShape(Circle())
+                                            .frame(height: 30)
+                                    })
+                                    
+                                    
+                                    Text(message.date)
+                                        .font(.subheadline)
+                                        .foregroundStyle(.tmGray)
+                                }
+                                
                             }
-                            .frame(width: 250)
-                            HStack{
-                                NavigationLink(destination: ProfileView(), label: {
-                                    Image(messageModel.user.profileImage)
-                                        .resizable()
-                                        .scaledToFit()
-                                        .clipShape(Circle())
-                                        .frame(height: 30)
-                                })
+                            .padding(EdgeInsets(top: 10, leading: 20, bottom: 10, trailing: 20))
+                            
+                        }
+                    }
+                    LazyVStack(alignment: .trailing){
+                        ForEach(sentList, id: \.self) { message in
+                            VStack(alignment: .trailing){
+                                ZStack(alignment: .leading){
+                                    RoundedRectangle(cornerSize: CGSize(width: 10, height: 10))
+                                        .foregroundStyle(.tmYellow2)
+                                    Text("\(message.message)")
+                                        .foregroundStyle(.tmBlack)
+                                        .padding(10)
+                                }
+                                .frame(width: 250)
                                 
-                                
-                                Text(message.date)
+                                Text("\(message.date)")
                                     .font(.subheadline)
                                     .foregroundStyle(.tmGray)
                             }
+                            .padding(EdgeInsets(top: 10, leading: 20, bottom: 10, trailing: 20))
                             
                         }
-                        .padding(EdgeInsets(top: 10, leading: 20, bottom: 10, trailing: 20))
-        
                     }
-                }
-                LazyVStack(alignment: .trailing){
-                    ForEach(sentList, id: \.self) { message in
-                        VStack(alignment: .trailing){
-                            ZStack(alignment: .leading){
-                                RoundedRectangle(cornerSize: CGSize(width: 10, height: 10))
-                                    .foregroundStyle(.tmYellow2)
-                                Text("\(message.message)")
-                                    .foregroundStyle(.tmBlack)
-                                    .padding(10)
-                            }
-                            .frame(width: 250)
-                            
-                            Text("\(message.date)")
-                                .font(.subheadline)
-                                .foregroundStyle(.tmGray)
-                        }
-                        .padding(EdgeInsets(top: 10, leading: 20, bottom: 10, trailing: 20))
-                        
-                    }
+                    VStack{
+                        Text("주의사항")
+                            .font(.subheadline)
+                            .foregroundStyle(.red)
+                        Text("채팅에서 욕설이나 폭언, 성적 수치심을 주는 행위를 하면 이용이 정지되거나\n관련 법령에 따라 처벌받을 수 있습니다.")
+                            .font(.caption)
+                            .foregroundStyle(.tmGray)
+                    }.padding(10)
                 }
             }
             .defaultScrollAnchor(.bottom)
